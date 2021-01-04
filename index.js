45c6c6fc87d0a38fbc84c375dd418375d6fcc9a6
김율희 <dbfgml741@likelion.org>
2021-01-04 20:59:07 +0900

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://yulkim:!khc5324@boilerplate.r3ldj.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected..'))
+  .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
